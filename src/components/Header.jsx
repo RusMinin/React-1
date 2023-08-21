48374027e6825b03c7b54d4e1f1c75a67818440a
Ruslan Minin <106439095+RusMinin@users.noreply.github.com>
2023-08-21 21:30:26 +0300

    Update Header.jsx
    modules2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
+import s from './Header.module.css' ;
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
     <img src='https://img.freepik.com/free-vector/colorful-letter-gradient-logo-design_474888-2309.jpg' alt='logo'></img>
   </header>
 }
