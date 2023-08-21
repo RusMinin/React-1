1e6fe880b8e65b2aeab10c5e6ac213d15f7f612c
Ruslan Minin <106439095+RusMinin@users.noreply.github.com>
2023-08-21 19:40:51 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://img.freepik.com/free-vector/colorful-letter-gradient-logo-design_474888-2309.jpg' alt='logo'></img>
+  </header>
+}
+
+export default Header;
