fe0b64319913d43f7de47155d1b12d0704288bbe
Ruslan Minin <106439095+RusMinin@users.noreply.github.com>
2023-08-21 21:30:48 +0300

    Update Navbar.jsx
    modules4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
+import s from './Navbar.module.css';
+
 
 const Navbar = () => {
-  return <nav className='nav'>
-    <div>
-      <a href='#'>Profile</a>
+  return <nav className={s.nav}>
+    <div className={s.item}>
+      <a>Profile</a>
     </div>
-    <div>
-      <a href='#'>Messages</a>
+    <div className={s.item}>
+      <a>Messages</a>
     </div>
-    <div>
-      <a href='#'>News</a>
+    <div className={`${s.item} ${s.active}`}>
+      <a>News</a>
     </div>
-    <div>
-      <a href='#'>Music</a>
+    <div className={s.item}>
+      <a>Music</a>
     </div>
-    <div>
-      <a href='#'>Settings</a>
+    <div className={s.item}>
+      <a>Settings</a>
     </div>
   </nav>
 }
