df49cbd067cb6d9f39a6fe99414aa1963ffb9e3e
Ruslan Minin <106439095+RusMinin@users.noreply.github.com>
2023-08-21 21:31:02 +0300

    Update Profile.jsx
    modules6

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
+import s from './Profile.module.css' ;
+
 
 const Profile = () => {
-  return <div className='content'>
+  return <div className={s.content}>
   <div>
     <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' alt='content'></img>
   </div>
@@ -12,11 +14,13 @@ const Profile = () => {
     <div>
       New Post
     </div>
-    <div>
+    <div className={s.posts}>
+    <div className={s.item}>
       post 1
     </div>
-    <div>
+    <div className={s.item}>
       post 2
+      </div>
     </div>
   </div>
 </div>
